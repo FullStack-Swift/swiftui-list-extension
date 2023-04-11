1c66081d9ff4f4fbd343a68f2201f4b13ba03d98
Nguyen Phong <nguyenphong.mobile.engineer@gmail.com>
2023-04-11 12:09:07 +0700

    fix mac os platform

--- Sources/swiftUIListExtension/swiftUIListExtension.swift
+++ Sources/swiftUIListExtension/swiftUIListExtension.swift
@@ -4,6 +4,7 @@ import Combine
 public extension View {
   @ViewBuilder
   func hideListRowSeperator() -> some View {
+#if os(iOS)
     if #available(iOS 15, *) {
       listRowSeparator(.hidden)
         .listRowInsets(EdgeInsets(.zero))
@@ -12,5 +13,18 @@ public extension View {
         .listRowInsets(EdgeInsets())
         .listRowInsets(EdgeInsets(top: -1, leading: -1, bottom: -1, trailing: -1))
     }
+#endif
+
+#if os(macOS)
+    if #available(macOS 13, *) {
+      listRowSeparator(.hidden)
+        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+    } else {
+      frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        .listRowInsets(EdgeInsets())
+        .listRowInsets(EdgeInsets(top: -1, leading: -1, bottom: -1, trailing: -1))
+    }
+
+#endif
   }
 }
